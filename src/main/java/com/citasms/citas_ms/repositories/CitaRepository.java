6d7d66a70ac30cdcbf3b24160cbc9fe4bd23b32a
titobriceno <titooswaldobriceno@gmail.com>
2021-12-10 23:49:11 -0500

    se agrega metodo para listar citas por profesional

--- src/main/java/com/citasms/citas_ms/repositories/CitaRepository.java
+++ src/main/java/com/citasms/citas_ms/repositories/CitaRepository.java
@@ -2,11 +2,10 @@ package com.citasms.citas_ms.repositories;
 import com.citasms.citas_ms.models.Cita;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.rmi.dgc.Lease;
 import java.util.List;
 
 public interface CitaRepository extends MongoRepository<Cita, String> {
-    //List <Cita> findByCitaOrigin (String citaOrigin);
+    List <Cita> getByProfesionalId(String profesionalId);
 
 
 }
